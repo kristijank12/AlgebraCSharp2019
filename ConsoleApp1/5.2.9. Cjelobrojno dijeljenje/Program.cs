371bb41551a2131c90beee231aa1882353391b93
kikijela12@gmail.com <kikijela12@gmail.com>
2019-02-26 18:41:47 +0100

    dodani zadaci

---
+++ ConsoleApp1/5.2.9. Cjelobrojno dijeljenje/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._9.Cjelobrojno_dijeljenje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+            int a = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ ConsoleApp1/Petlje_primjeri/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petlje_primjeri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            { Console.Write("{0}", i);
+
+            }
+            Console.WriteLine("");
+            //Ispis svih brojeva 16,18,20...88
+            for (int i = 16; i < 89; i -= 5)
+            {
+                Console.WriteLine("{0}", i);
+                for int
+            }
+        }
+    }
+}
