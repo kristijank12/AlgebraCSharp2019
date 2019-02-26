601b3a78131fc6db3c99a4e4d82fa1ce834ca084
kikijela12@gmail.com <kikijela12@gmail.com>
2019-02-26 16:26:17 +0100

    zadatak dodan

---
+++ ConsoleApp1/Zad 5.2.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_5._2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+            int a = int.Parse(Console.ReadLine());
+            if (a >= 100)
+            {
+                Console.WriteLine("Broj je velik.");
+            }
+            else
+            {
+                Console.WriteLine("broj nije velik.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
