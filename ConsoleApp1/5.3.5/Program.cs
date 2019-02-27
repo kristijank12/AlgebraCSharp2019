a6d5fa857e6e1c6ee28355a579a801a1fa051f49
kikijela12@gmail.com <kikijela12@gmail.com>
2019-02-27 18:15:56 +0100

    Sjajno

---
+++ ConsoleApp1/5.3.5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brojevi djeljivi s 3 iz intervala [3, 150} su;");
+            for (int i = 3; i <= 150; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("{0},", i);
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ ConsoleApp1/5.3.7. Ispis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._7.Ispis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brojevi djeljivi sa 7, a nedjeljivi s 3 iz intervala [1, 100} su;");
+            for (int i = 1;  i <= 100; i++)
+            {
+                if(i % 7== 0 && i % 3 != 0)
+                    {
+                    Console.Write("{0},", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
