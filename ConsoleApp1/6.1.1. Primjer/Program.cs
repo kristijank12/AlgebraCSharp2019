40e0aee6b1333a1ff28d6def47e80c07b47a22df
kikijela12@gmail.com <kikijela12@gmail.com>
2019-03-05 16:05:49 +0100

    Array

---
+++ ConsoleApp1/6.1.1. Primjer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._1.Primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklaracija
+            int[] niz = new int[3];
+
+            //int
+            niz[0] = 55;
+            niz[2] = 77;
+            niz[1] = 22;
+
+            for (int i = 0; i < niz.Length; i++)
+            {
+                if (niz[i] == null)
+
+                {
+                    throw new Exception("Niz sadrži ne iinicijaliziranu vrijednost na indexu");
+
+                }
+                Console.WriteLine((i+1)+"index");
+
+                Console.WriteLine((i + 1) +":" + niz[i]);
+                Console.ReadKey();
+            }
+
+
+        
+        }
+    }
+}
---
+++ ConsoleApp1/6.1.2. Max/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._2.Max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj;
+            int[] niz = new int[5];
+            for (int i = 0; i < niz.Lenght; i++) ;
+            {
+                if (niz[i] ==) niz.Max();
+            }
+            {
+                Console.WriteLine(niz.Max())
+
+            }
+            {
+                Console.ForegroundColor = ConsoleColor; // privremeno dohvaćam trenutnu boju
+                Console.Write("" + niz.Max());
+
+                Console.ForegroundColor = GCCollectionMode;
+
+            }
+
+        }
+    }
+}
