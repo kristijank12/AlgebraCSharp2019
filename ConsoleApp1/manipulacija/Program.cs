b381ba589aa99633e4e235ad7df9c264f2fc87c1
kikijela12@gmail.com <kikijela12@gmail.com>
2019-03-14 16:13:41 +0100

    Danas

--- ConsoleApp1/manipulacija/Program.cs
+++ ConsoleApp1/manipulacija/Program.cs
@@ -13,7 +13,7 @@ namespace manipulacija
         static void Main(string[] args)
         {
             Console.WriteLine("9.1.1. Znak u rijeci");
-            string rijec = "Danas je suncan dan dan";
+            string rijec = "Danas je suncan dan  dan dan";
             char slovo = 'n';
 
 
